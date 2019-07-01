d87b8ef9d897d2fb1870d026850c9fd5e376f182
smsuperman <gaojulin@hotmail.com>
2019-07-01 20:51:52 +0800

    2019.7.1-232,496

---
+++ Project/src/LeetCode496/Solution.java
@@ -0,0 +1,31 @@
+package LeetCode496;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * Solution
+ */
+public class Solution {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] result = new int[nums1.length];
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+
+            stack.push(num);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
