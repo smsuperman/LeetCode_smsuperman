01a11a22dc19859bd899684f46890f256dfcdead
smsuperman <gaojulin@hotmail.com>
2019-11-02 21:53:34 +0800

    2019.11.2-11

---
+++ Project/src/LeetCode11/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode11;
+
+/**
+ * Solution
+ */
+public class Solution {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+
+        for(int i = 0;i<height.length;i++){
+            for(int j = i+1;j<height.length;j++){
+                max = Math.max(max,Math.min(height[i], height[j])*(j-i));
+            }
+        }
+
+        return max;
+    }
+}
